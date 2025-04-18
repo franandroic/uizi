b5628be78ed97ecd4309e38b1711db923a5da030
fran <fran.androic@fer.hr>
2025-04-18 14:03:10 +0200

    bugfix

--- sources/cursor.cpp
+++ sources/cursor.cpp
@@ -11,27 +11,28 @@ Cursor::Cursor(int mx, int my) {
 
 void Cursor::move(bool direction, int distance) {
 
-    //if (distance != 1 && distance != -1) return;
+    if (distance != 1 && distance != -1) return;
 
     if (direction) {
 
-        /*
         if ((distance > 0 && x >= maxX - 1) ||
             (distance < 0 && x <= 0)) {
             return;
         }
-        */
+
         x += distance;
+        std::cout << "X DISTANCE" << std::endl;
 
     } else {
         
-        /*
         if ((distance > 0 && y >= maxY - 1) ||
             (distance < 0 && y <= 0)) {
             return;
         }
-        */
+
         y += distance;
+
+        std::cout << "Y DISTANCE" << std::endl;
     }
 
 }
